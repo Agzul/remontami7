971b2baeff5be07991321b68061a33209eeda944
oag2 <Agzul>
2018-08-18 20:56:12 +0300

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,14 +1,4 @@
 Rails.application.configure do
-  # Use environment names or environment variables:
-  # break unless Rails.env.production?
-  break unless ENV['ENABLE_COMPRESSION'] == '1'
-
-  # Strip all comments from JavaScript files, even copyright notices.
-  # By doing so, you are legally required to acknowledge
-  # the use of the software somewhere in your Web site or app:
-  uglifier = Uglifier.new output: { comments: :none }
-  # To keep all comments instead or only keep copyright notices (the default):
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -34,38 +24,11 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor  = :uglifier
+  config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
 
-  config.middleware.use Rack::Deflater
-  config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
-  config.middleware.use HtmlCompressor::Rack,
-    compress_css: true,
-    compress_javascript: true,
-    css_compressor: Sass,
-    enabled: true,
-    javascript_compressor: uglifier,
-    preserve_line_breaks: false,
-    remove_comments: true,
-    remove_form_attributes: false,
-    remove_http_protocol: false,
-    remove_https_protocol: false,
-    remove_input_attributes: true,
-    remove_intertag_spaces: false,
-    remove_javascript_protocol: true,
-    remove_link_attributes: true,
-    remove_multi_spaces: true,
-    remove_quotes: true,
-    remove_script_attributes: true,
-    remove_style_attributes: true,
-    simple_boolean_attributes: true,
-    simple_doctype: false
-
-
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-  config.assets.debug = false
-
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
