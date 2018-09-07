0adfe906d439ddbd6d762af9959112bcede67869
oag2 <Agzul>
2018-09-07 20:40:19 +0300

    fix

--- config/application.rb
+++ config/application.rb
@@ -13,18 +13,14 @@ module Remontami7
     config.load_defaults 5.1
 
     ENV['RACK_ENV'] ||= 'development'
-
     if ENV['RACK_ENV'] == 'development'
       ENV['SITE_URL'] = 'localhost:3000'
       else
       ENV['SITE_URL'] = 'remontami7.herokuapp.com'
     end
-
     config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
       r301 %r{^(.+)/$}, '$1' # 301 редирект, где в конце ссылок любое количество /
-      r301 %r{.*}, "http://#{ENV['SITE_URL']}$&", :if => Proc.new { |rack_env| rack_env['SERVER_NAME'].start_with?('www')} # 301 редирект, с www на без www
-
-
+      r301 %r{.*}, "https://#{ENV['SITE_URL']}$&", :if => Proc.new { |rack_env| rack_env['SERVER_NAME'].start_with?('www')} # 301 редирект, с www на без www
     end
 
     # Settings in config/environments/* take precedence over those specified here.
