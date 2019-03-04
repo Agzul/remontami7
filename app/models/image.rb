c325e79e0a65cfbe09f1437fe74cdc15ed067a6c
Agzul <amayak007@mail.ru>
2019-03-04 20:17:21 +0300

    justify all, about h1-6, resized small images

--- app/models/image.rb
+++ app/models/image.rb
@@ -8,21 +8,21 @@ class Image < ApplicationRecord
     mobile: [ "240x?", :jpeg ]
   },
   convert_options: {
-    all:     "-quality 85 -strip"
+    all: "-quality 85 -strip"
   }
 
   has_attached_file :logo, styles: {
    logo: [ "?x64", :jpeg ]
   },
   convert_options: {
-    all:  "-quality 85 -strip"
+    all: "-quality 85 -strip"
   }
 
   has_attached_file :tagline, styles: {
-    med:   [ "250x32!" ]
+    med: [ "250x32!" ]
   },
   convert_options: {
-    all:  "-quality 85 -strip"
+    all: "-quality 85 -strip"
   }
 
   has_attached_file :gallery_image, styles: {
@@ -39,22 +39,13 @@ class Image < ApplicationRecord
     mobile_cover: "-background black -gravity center -extent 280x280"
   }
 
-  has_attached_file :info_image, styles: {
-   thumb: [ "100x75!", :jpeg ],
-   small: [ "180x?",   :jpeg ],
-   med:   [ "360x?",   :jpeg ]
-  },
-  convert_options: {
-    all:  "-quality 85 -strip"
-  }
-
   has_attached_file :slider_image, styles: {
     pc:     [ "720x360!", :jpeg ],
     tablet: [ "480x240!", :jpeg ],
     mobile: [ "320x160!", :jpeg ]
   },
   convert_options: {
-    all:  "-quality 85 -strip"
+    all: "-quality 85 -strip"
   }
 
   has_attached_file :static_image, styles: {
@@ -62,25 +53,32 @@ class Image < ApplicationRecord
     med:   [ "160x160!" ]
   },
   convert_options: {
-    all:  "-quality 85 -strip"
+    all: "-quality 85 -strip"
+  }
+
+  has_attached_file :info_image, styles: {
+    thumb: [ "100x75!", :jpeg ],
+    small: [ "320x?",   :jpeg ],
+    med:   [ "360x?",   :jpeg ]
+  },
+  convert_options: {
+    all: "-quality 85 -strip"
   }
 
   has_attached_file :vid_remonta_image, styles: {
-    thumb: [ "100x75!", :jpeg ],
-    small: [ "180x?",   :jpeg ],
+    small: [ "320x?",   :jpeg ],
     med:   [ "360x?",   :jpeg ]
-   },
+  },
   convert_options: {
-    all:  "-quality 85 -strip"
+    all: "-quality 85 -strip"
   }
 
   has_attached_file :vid_rabot_image, styles: {
-    thumb: [ "100x75!", :jpeg ],
-    small: [ "180x?",   :jpeg ],
+    small: [ "320x?",   :jpeg ],
     med:   [ "360x?",   :jpeg ]
-   },
+  },
   convert_options: {
-    all:  "-quality 85 -strip"
+    all: "-quality 85 -strip"
   }
 
   validates_attachment :background, :logo, :tagline, :gallery_image, :info_image, :slider_image, :static_image, :vid_remonta_image, :vid_rabot_image, content_type: {
